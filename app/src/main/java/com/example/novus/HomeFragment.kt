5dd8eda70a0bd09f04b184e94bd7ab309f093f1a
Nathan-spec <nathankkemboi@gmail.com>
2022-07-25 14:42:49 +0300

    First commit

---
+++ app/src/main/java/com/example/novus/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.novus
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+}
---
+++ app/src/main/java/com/example/novus/SettingsFragment.kt
@@ -0,0 +1,16 @@
+package com.example.novus
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+}
